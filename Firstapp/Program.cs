1a1af50d13f57447b83e2ca2f01e907e21ad7a22
Elmar_Kouliev <ker1912@mail.ru>
2022-09-15 23:57:37 +0300

    Online Lessons

---
+++ Firstapp/Program.cs
@@ -0,0 +1,5 @@
+﻿//int a;
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = a*a;
+Console.WriteLine("квадрат числа" + " = " + res);
---
+++ Secondapp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите квадрат числа");
+int S = Convert.ToInt32(Console.ReadLine());
+if (S == a*a)
+{
+    Console.WriteLine("Верно");
+}
+else 
+Console.WriteLine("Попробуйте другое");
