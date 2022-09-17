6c6ac9d00a7023d393e2c0e65ff5a2531e343ca5
Elmar_Kouliev <ker1912@mail.ru>
2022-09-17 22:55:07 +0300

    Задача с днями недели

---
+++ Task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿// 1. Задача про дни недели или сказка про белого бычка
+Console.Write("Введите номер дня (1-7): ");
+string DayNumber = Console.ReadLine();
+if(DayNumber == "1")
+{
+    Console.WriteLine("Это Пн");
+}
+else
+{
+    if(DayNumber == "2")
+    {
+        Console.WriteLine("Это Вт");
+    }
+    else
+    {
+        if(DayNumber == "3")
+        {
+            Console.WriteLine("Это Ср");
+        }
+        else
+        {
+            if(DayNumber == "4")
+            {
+                Console.WriteLine("Это Чт");
+            }
+            else
+            {
+                if(DayNumber == "5")
+                {
+                    Console.WriteLine("Это Пт");
+                }
+                else
+                {
+                    if(DayNumber == "6")
+                    {
+                        Console.WriteLine("Это Сб");
+                    }
+                    else
+                    {
+                        if(DayNumber == "7")
+                        {
+                            Console.WriteLine("Это Вс");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Вы ввели некорректное число :(");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
