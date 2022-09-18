19a89fdbe7adea363d858e7e178b70d800a97a45
Elmar_Kouliev <ker1912@mail.ru>
2022-09-18 10:43:13 +0300

    Задача 2 макс

---
+++ Task_2_max/Program.cs
@@ -0,0 +1,16 @@
+﻿// На вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a, b, Max;
+Console.WriteLine("Введите число");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+b = int.Parse(Console.ReadLine());
+if(a >= b)
+{
+    Max = a;
+}
+else
+{
+    Max = b;
+}
+Console.Write("max = ");
+Console.Write(Max);
