2c1c195a444433dd28c9f95968dd4d6d7c039b85
Elmar_Kouliev <ker1912@mail.ru>
2022-09-18 22:07:08 +0300

    Задача 4 максимум

---
+++ Task_4_max/Program.cs
@@ -0,0 +1,12 @@
+﻿// Принимает на вход "n" чисел и выдаёт максимальное из этих чисел
+decimal Max = 0, a = 0;
+int i = 0, n = 3;
+while(i < n)
+{
+    Console.Write("Введите число:");
+    a = Convert.ToDecimal(Console.ReadLine());
+    if(a > Max) Max = a;
+    i++;
+}
+Console.Write($"Максимальное число: {Max}");
+
