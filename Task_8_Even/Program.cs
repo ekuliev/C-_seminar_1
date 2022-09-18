4ccf874bbfcf710f11d266dd433d91386807cd8a
Elmar_Kouliev <ker1912@mail.ru>
2022-09-18 23:44:56 +0300

    Задача 8 чётн 1-N

---
+++ Task_8_Even/Program.cs
@@ -0,0 +1,9 @@
+﻿// на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+int i = 1;
+Console.Write("Введите число:");
+int? N = int.Parse(Console.ReadLine());
+while (i <= N)
+{
+    if (i% 2 == 0) Console.Write($"{i} ");
+    i++;
+}
