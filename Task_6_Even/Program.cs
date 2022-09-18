bb501a4069b8192f4850ec93248f73bf238e777e
Elmar_Kouliev <ker1912@mail.ru>
2022-09-18 22:41:40 +0300

    Задача 6 на четность

---
+++ Task_6_Even/Program.cs
@@ -0,0 +1,5 @@
+﻿// на вход принимает число и выдаёт, является ли число чётным
+Console.Write("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 0) Console.Write("Чётное");
+else Console.Write("Нечётное");
